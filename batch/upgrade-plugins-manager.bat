b3ed4a19bdd50527bd6b94a65b754426f7010c1d
fuzzzzzz <fuzzzzzz@gmail.com>
2019-11-17 13:57:39 +0100

    initial commit

---
+++ batch/upgrade-plugins-manager.bat
@@ -0,0 +1,4 @@
+:: zCycler
+
+:: Upgrade plugins_manager in Zbrush 2020\ZStartup\Zplug64 directory
+xcopy /s /y "L:\programming\WORKSPACES\zbrush-plugins-manager\_plugins_manager_2020.txt" "D:\Imaging\Pixologic\ZBrush 2020\ZStartup\ZPlugs64\" /f
