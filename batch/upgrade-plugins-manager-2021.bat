f4a9fc75c250d05f983b551f504bc183f40f17d4
fuzzzzzz <fuzzzzzz@gmail.com>
2020-08-14 04:33:37 +0200

    Adding ZBrush 2021 support

---
+++ batch/upgrade-plugins-manager-2021.bat
@@ -0,0 +1,4 @@
+:: zCycler
+
+:: Upgrade plugins_manager in Zbrush 2020\ZStartup\Zplug64 directory
+xcopy /s /y "L:\programming\WORKSPACES\zbrush-plugins-manager\_Plugins_Manager_2021.txt" "D:\Imaging\Pixologic\ZBrush 2021\ZStartup\ZPlugs64\" /f
