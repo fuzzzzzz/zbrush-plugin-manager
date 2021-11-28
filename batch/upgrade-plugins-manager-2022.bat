90828eba308f59b2a6f58b385e705f9dd54d618f
facelessmindz <fuzzzzzz@gmail.com>
2021-11-28 05:41:45 +0100

    add new version (ZBrush 2022)

---
+++ batch/upgrade-plugins-manager-2022.bat
@@ -0,0 +1,4 @@
+:: zCycler
+
+:: Upgrade plugins_manager in Zbrush 2020\ZStartup\Zplug64 directory
+xcopy /s /y "L:\programming\WORKSPACES\zbrush-plugins-manager\_Plugins_Manager_2022.txt" "D:\Imaging\Pixologic\ZBrush 2022\ZStartup\ZPlugs64\" /f
